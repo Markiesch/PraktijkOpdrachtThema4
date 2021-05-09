90417e7cce7f42488986909a962e2700ae63ab78
Markiesch <marksmeel@gmail.com>
2021-05-09 13:58:25 +0200

    Backup release

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <title>Home · BTC de Pettelaer</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <img src="images/logo@2x.png" alt="Pettelaer Logo">
+                </li>
+                <li>
+                    <a href="">Inloggen</a>
+                    <input type="text">
+                </li>
+            </ul>
+        </nav>
+        <div>
+            
+        </div>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
