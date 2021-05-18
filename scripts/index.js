eeed5283ec6b92d03042bac306ed21b887db9a80
Markiesch <marksmeel@gmail.com>
2021-05-18 20:55:46 +0200

    Added page transition

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
-const logInScreen = document.querySelector(".login--container");
-const exitBtn = document.querySelector(".exit-btn");
+const bubble = document.querySelector(".bubble");
 const link = document.querySelector(".inlog--link");
+const transitionTime = 600;
 
-link.addEventListener("click", toggleMenu);
-exitBtn.addEventListener("click", toggleMenu);
+link.addEventListener("click", (e) => {
+    e.preventDefault();
 
-function toggleMenu() {
-    logInScreen.classList.toggle("open");
+    bubble.style.left = `${e.clientX}px`;
+    bubble.style.top = `${e.clientY}px`;
+    bubble.style.width = `0`;
+    bubble.style.height = `0`;
+
+    dotEnlarge();
+    transitionEnd();
+});
+
+function dotEnlarge() {
+    setTimeout(() => {
+        bubble.style.transition = `${transitionTime}ms ease-in-out all`;
+        bubble.style.width = `125vw`;
+        bubble.style.height = `125vw`;
+        bubble.style.left = `50%`;
+        bubble.style.top = `50%`;
+    }, 10);
+}
+
+function transitionEnd() {
+    setTimeout(() => {
+        window.location = "pages/login.php";
+    }, transitionTime * 1.5);
 }
